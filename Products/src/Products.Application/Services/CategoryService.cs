6f4693b0bc0c8673675e6a3009b672fcfc87ba56
Eşref Batuhan Simsar <110763853+batuhansimsar@users.noreply.github.com>
2025-10-19 15:55:47 +0300

    Add Services to Application layer

---
+++ Products/src/Products.Application/Services/CategoryService.cs
@@ -0,0 +1,48 @@
+using Products.DTO;
+using Products.Models;
+using Products.Data;
+using AutoMapper;
+
+namespace Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        public CategoryService(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<CategoryDto> GetAllCategories()
+        {
+            var categories = _context.Categories.ToList();
+            return _mapper.Map<List<CategoryDto>>(categories);
+        }
+
+        public CategoryDto? GetCategoryById(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            return category == null ? null : _mapper.Map<CategoryDto>(category);
+        }
+
+        public CategoryDto AddCategory(CategoryCreateDto categoryDto)
+        {
+            var category = _mapper.Map<Category>(categoryDto);
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return _mapper.Map<CategoryDto>(category);
+        }
+
+        public void DeleteCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+        }
+    }
+} 
