2dd1f170872479118d969419d85ea4ae5dba4040
Eşref Batuhan Simsar <110763853+batuhansimsar@users.noreply.github.com>
2025-10-19 15:56:40 +0300

    Add files via upload

---
+++ Products/src/Products.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using Products.Models;
+using Products.DTO;
+
+namespace Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+           
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Product, ProductCreateDto>().ReverseMap();
+            CreateMap<Product, ProductUpdateDto>().ReverseMap(); 
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryCreateDto>().ReverseMap();
+          
+            
+        }
+    }
+} 
